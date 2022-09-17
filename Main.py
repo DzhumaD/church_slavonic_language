64f64709dd39a686cdf40620a6c96357e3f7cfaa
Dzhuma <dzhumashop@gmail.com>
2022-09-17 16:55:16 +0500

    start

---
+++ Main.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import Tk, font
+import pyglet, os
+import tkinter as tk
+##############  Добавление шрифта  ##########################
+pyglet.font.add_file('PonomarUnicode.otf')  # Your TTF file name here
+
+# root = tk.Tk()
+# MyLabel = tk.Label(root,text="ф",font=('PonomarUnicode',25))
+#
+# MyLabel.pack()
+# root.mainloop()
+#############################################################
+
+units = {}  #словарь цифири 1 - 10
+dozens = {} #словарь десятков
+
+# спросить какое это число в цифири
+# варианты ответов
+# считать ответ
+# вывести результат
+
+window = Tk()
+window.title("Церковно-славянский язык")
+
+window.geometry('400x250')
+lbl = Label(window, text="Привет", font=("PonomarUnicode", 50))
+lbl.grid(column=0, row=0)
+btn = Button(window, text="Не нажимать!")
+btn.grid(column=1, row=0)
+font.families()
+print(font.families())
+window.mainloop()
